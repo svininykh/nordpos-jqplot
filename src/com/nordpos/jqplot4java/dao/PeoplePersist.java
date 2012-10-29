d661dee037faec950bf600400ac830933f5758b8
Andrey Svininykh <svininykh@gmail.com>
2012-10-29 17:34:39 +0600

    Add persist library.

---
+++ src/com/nordpos/jqplot4java/dao/PeoplePersist.java
@@ -0,0 +1,54 @@
+/**
+ *  Copyright (c) 2012 Nord Trading Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.nordpos.jqplot4java.dao;
+
+import com.nordpos.jqplot4java.model.People;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.sf.persist.Persist;
+
+/**
+ *
+ * @author Andrey Svininykh <svininykh@gmail.com>
+ */
+public class PeoplePersist extends BaseJDBCPersist {
+
+    public PeoplePersist() {
+        try {
+            persist = new Persist(getConnection());
+        } catch (Exception ex) {
+            Logger.getLogger(PeoplePersist.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public People findUser(String id) {
+        return persist.read(People.class,
+                "SELECT * FROM PEOPLE WHERE ID = ?",
+                id);
+    }
+
+    public List<People> findUsers() {
+        return persist.readList(People.class,
+                "SELECT * FROM PEOPLE ORDER BY NAME");
+    }
+
+    public Integer countUsers() {
+        return persist.read(Integer.class,
+                "SELECT COUNT(ID) FROM PEOPLE");
+    }
+}
---
+++ src/com/nordpos/jqplot4java/model/People.java
@@ -0,0 +1,88 @@
+/**
+ *  Copyright (c) 2012 Nord Trading Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.nordpos.jqplot4java.model;
+
+/**
+ *
+ * @author Andrey Svininykh <svininykh@gmail.com>
+ */
+public class People {
+
+    private String id;
+    private String name;
+    private String apppasword;
+    private String card;
+    private String role;
+    private Boolean visible;
+    private Byte[] image;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAppPassword() {
+        return apppasword;
+    }
+
+    public void setAppPassword(String apppasword) {
+        this.apppasword = apppasword;
+    }
+
+    public String getCard() {
+        return card;
+    }
+
+    public void setCard(String card) {
+        this.card = card;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Boolean getVisible() {
+        return visible;
+    }
+
+    public void setVisible(Boolean visible) {
+        this.visible = visible;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+}
